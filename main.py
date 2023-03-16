82afdd3f723bab613d112edaa1e18530c208210b
Taras-js <78747117+Taras-js@users.noreply.github.com>
2023-03-17 00:07:50 +0300

    feat: keyboard and content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import telebot
 import config
-import random
 
 from telebot import types
 
@@ -11,13 +10,13 @@ bot = telebot.TeleBot(config.BOT_TOKEN)
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item_one = types.KeyboardButton("CV in Russian")
-    item_two = types.KeyboardButton("Experience")
-    item_free = types.KeyboardButton("CV in English")
-    item_foo = types.KeyboardButton("Project")
-    item_five = types.KeyboardButton("Skills")
-    item_six = types.KeyboardButton("Company")
+    item_two = types.KeyboardButton("Projects")
+    item_free = types.KeyboardButton("Github")
+    item_foo = types.KeyboardButton("CV in English")
+    item_five = types.KeyboardButton("Gitlab")
+    item_six = types.KeyboardButton("Skills")
     markup.add(item_one, item_two, item_free, item_foo, item_five, item_six)
-    sti = open('static/photo_2022-12-31_13-06-16.jpg', 'rb')
+    sti = open('static/images.jpg', 'rb')
     bot.send_sticker(message.chat.id, sti)
     bot.send_message(message.chat.id, 'Welcome, {0.first_name}!'.format(message.from_user, bot.get_me()),
                      parse_mode="html", reply_markup=markup)
@@ -29,12 +28,21 @@ def output(message):
         if message.text == 'CV in Russian':
             cv = open('static/Taras.pdf', 'rb')
             bot.send_document(message.chat.id, cv)
-        elif message.text == 'Experience':
-            bot.send_message(message.chat.id, 'Anybots Bali, anybots.ru'
-                                              'интеграция, интернет'
-                                              'Full-stack разработчик (full-time)'
-                                              '1. разработка телеграмм ботов на '
-                                              'node.js'.format(message.from_user, bot.get_me(), parse_mode="markup"))
+        if message.text == 'CV in English':
+            cv = open('static/Taras_cv.pdf', 'rb')
+            bot.send_document(message.chat.id, cv)
+        if message.text == 'Projects':
+            bot.send_message(message.chat.id, f'site: https://taras-server.ru/  \n bot: https://t.me/Demo_auto_bot'
+                             .format(message.from_user, bot.get_me(), parse_mode="markup"))
+        if message.text == 'Github':
+            bot.send_message(message.chat.id, 'https://github.com/Taras-js'.format(message.from_user, bot.get_me(),
+                                                                                   parse_mode="markup"))
+        if message.text == 'Skills':
+            cv = open('static/scills.webp', 'rb')
+            bot.send_document(message.chat.id, cv)
+        if message.text == 'Gitlab':
+            bot.send_message(message.chat.id, 'https://gitlab.com/Taras-JS'.format(message.from_user, bot.get_me(),
+                                                                                   parse_mode="markup"))
 
 
 bot.polling(none_stop=True)
